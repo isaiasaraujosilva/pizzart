7c6e9f375dab9fe61a456869b99798b4211ece15
isaias-araujo1@hotmail.com <isaiasaraujopi@gmail.com>
2022-05-21 17:12:55 -0300

    connecting mysql

--- pizzart/pizzart/settings.py
+++ pizzart/pizzart/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'pizzart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_pizzaria_joao',
+        'USER': 'root',
+        'PASSWORD': 'vamos@12',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
