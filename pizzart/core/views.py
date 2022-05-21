c55bda1cc239316a97544edb8f90981ab1b33632
isaias-araujo1@hotmail.com <isaiasaraujopi@gmail.com>
2022-05-21 19:28:36 -0300

    change , select index.html

--- pizzart/core/views.py
+++ pizzart/core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from core.models import Massas
 
-class index(TemplateView):
-    template_name='index.html'
+#class IndexPageViwer(TemplateView):
+#    template_name='index.html'
+
+def index(request):
+    massa = Massas.objects.all()
+    return render (request,'index.html',{
+        'massa':massa
+    })
