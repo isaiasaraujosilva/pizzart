a69b5b950e27226d694bf46ea207f03768711097
isaias-araujo1@hotmail.com <isaiasaraujopi@gmail.com>
2022-05-21 18:02:49 -0300

    Install app core

--- pizzart/pizzart/urls.py
+++ pizzart/pizzart/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from core import urls
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
