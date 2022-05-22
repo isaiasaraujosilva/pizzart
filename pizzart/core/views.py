375ea3453ba2439a96bf5e26a1df5dd639fa10b1
isaias-araujo1@hotmail.com <isaiasaraujopi@gmail.com>
2022-05-22 09:40:15 -0300

    populating index selects

--- pizzart/core/views.py
+++ pizzart/core/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from core.models import Massas
+from core.models import Massas, Bordas, Sabores
 
 #class IndexPageViwer(TemplateView):
 #    template_name='index.html'
 
 def index(request):
     massa = Massas.objects.all()
+    borda= Bordas.objects.all()
+    sabor = Sabores.objects.all()
     return render (request,'index.html',{
-        'massa':massa
+        'massa':massa,
+        'borda':borda,
+        'sabor':sabor,
     })
